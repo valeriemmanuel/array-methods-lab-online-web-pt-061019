9b210f30d7898fbbfe1c165be9db8fc4c70ac1eb
Valerie Manuel <valerie.manuel11@gmail.com>
2019-06-23 00:00:59 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -2,12 +2,12 @@ def using_include(array, element)
 array.include? (element)
 end
 
-def using_sort("Val", "Ma", "Uncle")
+def using_sort(array)
  using_sort.sort
 end
 
 def using_reverse(array)
-
+  using_reverse.reverse
 end
 
 def using_first(array)
